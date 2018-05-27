ba8653130e7ba51e6fbbe483aa873ea56fa5e831
Valdemar Ceccon <valdemar@localhost.localdomain>
2018-05-27 15:02:00 -0300

    Hello world

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,18 +1,5 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Cards.hello
-      :world
-
-  """
   def hello do
-    :world
+    "Hi there!"
   end
 end
