9930312096220054144319a946460aedb064dd0d
Valdemar Ceccon <valdemar@localhost.localdomain>
2018-05-27 22:22:58 -0300

    Added and implemented create_hand

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -33,4 +33,10 @@ defmodule Cards do
       {:error, _reason} -> "That file does not exists"
     end
   end
+
+  def create_hand(hand_size) do
+    deck = Cards.create_deck
+    deck = Cards.shuffle(deck)
+    hand = Cards.deal(deck, hand_size)
+  end
 end
