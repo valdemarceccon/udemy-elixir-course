f9e3f41223f9fa46cfcb5ef007b6d5386a8fa3fc
Valdemar Ceccon <valdemar@localhost.localdomain>
2018-05-28 00:27:11 -0300

    pickcolor now returns a list with rgb color to be used in the image.

--- identicon/lib/identicon.ex
+++ identicon/lib/identicon.ex
@@ -2,6 +2,13 @@ defmodule Identicon do
   def main(input) do
     input
     |> hash_string
+    |> pick_color
+  end
+
+  def pick_color(image) do
+    %Identicon.Image{hex: [red, green, blue | _tail]} = image
+    [red, green, blue]
+    tail
   end
 
   def hash_string(input) do
